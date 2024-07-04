c175860efd24489f238dda21bf81c9c3f9e8aaea
kailyn <kailyn.crawford1996@gmail.com>
2024-07-03 21:29:56 -0500

    wednesday night activities

---
+++ section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a variable called "name" that references a string
+name = "kailyn";
+// create a variable called "favoriteSong" that references a string
+
+// create a variable called "wage" that references a number
+let wage = "hotel california";
+// create a variable called "age" that references a number
+let age = 21;
+// create a variable called "onlyChild" that references a boolean
+const onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing ="1975 Ford F350";
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favsong = 'the streets have no name';
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = false;
+favoriteThing = false;
+// Happy Birthday!
+// Reassign "age" to a new value
+age = age + 1
+age = 22;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage =wage * 2;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console ("my favorite is"+ favsong +" and my age is" + wage)
---
+++ section02/2.2/2.2.3-JSMorePrimitives.js
@@ -0,0 +1,23 @@
+// Create a variable "timeOfDay" and assign it a string
+let timeOfDay = "noon";
+// Create a variable "greeting" that references a template literal
+// Use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+let greeting = 'we will be eating lunch at  ${timeOfDay}';
+// Print "greeting"
+console.log {gretting};
+// Create a new variable, but do not assign it a value
+let blank;
+// Print the new variable and its type
+// What type should we expect?
+let blank;
+// Assign the variable a value that indicates the variable is purposely blank
+// What value should we assign?
+
+// Print the variable and its type again
+// What type should we expect?
+
+// Try to print a variable that does not exist
+// What should we expect to print in the CLI?
+
+// Print "greeting" again
+// Will this line run?
